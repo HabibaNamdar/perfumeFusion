bb322b6c26665af6322bf6c86c54b64bd808402e
Habiba Namdar <habiba.namdar.awkum@gmail.com>
2024-02-19 21:39:07 +0500

    we apply validation on login and registor

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,22 +1,13 @@
 import { Link } from "react-router-dom";
 import "./login.css";
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useState } from "react";
+import {  FieldValues, useForm } from "react-hook-form";
+
 function Login() {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const person = { email: "", password: "" };
-  
-  const handlerSumbit = (event: FormEvent) => {
-    event.preventDefault();
-  
-    if (emailRef.current) {
-      person.email = emailRef.current.value;
-    }
-    if (passwordRef.current) {
-      person.password = passwordRef.current.value;
-    }
-    console.log(person);
-  };
+
+const {register, handleSubmit, formState: {errors}} = useForm();
+
+const onSubmit = (data: FieldValues) =>console.log(data);
 
   return (
     <div className="container  mt-3">
@@ -27,29 +18,29 @@ function Login() {
               <h1 className="login-title">LOGIN</h1>
             </div>
           </div>
-          <form onSubmit={handlerSumbit}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="row mt-3">
               <div className="col-12">
                 <div className="mb-3">
                   <label className="form-label">Email address</label>
-                  <input
+                  <input {...register('email', {required:true})}
                     type="email"
-                    ref={emailRef}
                     className="form-control"
                     placeholder="name@example.com"
                   />
-  
+                  {errors.email?.type === 'required' &&<p className="text-danger"> email is required.</p>}
+                  
                 </div>
                 <div className="row">
                   <div className="col-12">
                     <div className="mb-3">
                       <label className="password">Password</label>
                       <label className="forgotpassword">Forgot password?</label>
-                      <input
-                        ref={passwordRef}
+                      <input {...register('password', {required:true})}
                         type="password"
                         className="form-control"
                       />
+                      {errors.password?.type === 'required' &&<p className="text-danger">password is required</p>}
                     </div>
                     <div className="row">
                       <div className="col-12">
--- src/components/registor/Registor.tsx
+++ src/components/registor/Registor.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import "./register.css";
+import {  FieldValues, useForm } from "react-hook-form";
 function Registor() {
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
+const onSubmit = (data: FieldValues) =>console.log(data);
+
   return (
     <div className="container mt-3">
       <div className="row justify-content-center">
@@ -10,35 +15,40 @@ function Registor() {
               <h1 className="create-account">CREATE ACCOUNT</h1>
             </div>
           </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row mt-3">
             <div className="col-12">
               <div mb-3>
                 <label className="firstname mb-2">First Name</label>
-                <input type="text" className="form-control" />
+                <input {...register('firstname' , {required:true})} type="text" className="form-control" />
+                {errors.firstname?.type === 'required' && <p className="text-danger">firstname is required.</p>}
               </div>
               <div className="row mt-3">
                 <div className="col-12">
                   <div mb-3>
                     <label className="lastname mb-2">Last Name</label>
-                    <input type="text" className="form-control" />
+                    <input {...register('lastname', {required:true})} type="text" className="form-control" />
+                    {errors.lastname?.type === 'required' && <p className="text-danger">lastname is required.</p>}
                   </div>
                   <div className="row mt-3">
                     <div className="col-12">
                       <div mb-3>
                         <label className="email mb-2">Email address</label>
-                        <input type="email" className="form-control" />
+                        <input {...register('email', {required:true})} type="email" className="form-control" />
+                       {errors.email?.type === 'required' && <p className="text-danger">email is required.</p>}
                       </div>
                       <div className="row mt-3">
                         <div className="col-12">
                           <div mb-3>
                             <label className="Password mb-2">Password</label>
-                            <input type="text" className="form-control" />
+                            <input {...register('password', {required:true})} type="text" className="form-control" />
+                          {errors.password?.type === 'required' &&  <p className="text-danger">password is required.</p>}
                           </div>
                           <div className="row mt-3">
                             <div className="col-12">
                               <div mb-2>
                                 <button
-                                  type="button"
+                                  type="submit"
                                   className="btn btn-primary btn-lg btn-block col-12 login-btn mb-2"
                                 >
                                   Create
@@ -59,6 +69,7 @@ function Registor() {
               </div>
             </div>
           </div>
+          </form>
         </div>
       </div>
     </div>
